1c0313fb68d381d2650a4b0a212b46fd41889122
ViniMagaa <vinisantos2008vs@gmail.com>
2024-01-29 00:35:47 -0300

    feat: seção por que Faveni

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// --- Menu ---
 const burgerButton = document.querySelector("#burger");
 const burgerIcon = burgerButton.querySelector("i"); // Assuming the icon is an <i> element
 const menu = document.querySelector("#menu");
@@ -18,13 +19,65 @@ const toggleMenu = () => {
 	menu.classList.contains("active") ? deactivateMenu() : activateMenu();
 };
 
-const bodyClickHandler = (e) => {
+// --- Carousels ---
+const carouselPrevButton = document.getElementById(
+	"why-faveni-carousel-button-left"
+);
+const carouselNextButton = document.getElementById(
+	"why-faveni-carousel-button-right"
+);
+const carouselWhyFaveni = document.getElementById("why-faveni-carousel");
+
+const carouselWhyFaveniScroll = (direction) => {
+	const width = carouselWhyFaveni.style.width.replace("px", "");
+	carouselScrollX(direction * Number(width));
+};
+
+const updateCarouselsItemsWidth = (width) => {
+	const items = document.getElementsByClassName("carousel-item");
+	const inner = document.getElementsByClassName("carousel-inner");
+
+	inner[0].style.width = items.length * 0.7 * width + "px";
+};
+
+const updateWhyFaveniCarouselWidth = () => {
+	const width = window.innerWidth < 700 ? window.innerWidth : 700;
+	carouselWhyFaveni.style.width = width + "px";
+
+	updateCarouselsItemsWidth(width);
+};
+
+const carouselScrollX = (width) => {
+	carouselWhyFaveni.scrollTo(carouselWhyFaveni.scrollLeft + width, 0);
+};
+
+// --- Global Handlers ---
+const clickHandler = (e) => {
 	if (!menu.contains(e.target) && !burgerButton.contains(e.target)) {
 		deactivateMenu();
 	}
 };
 
+const resizeHandler = () => {
+	updateWhyFaveniCarouselWidth();
+};
+
+// --- Events ---
+updateWhyFaveniCarouselWidth();
+
+// --- Event Listeners ---
+
+// Carousel
+carouselPrevButton.addEventListener("click", () =>
+	carouselWhyFaveniScroll(-0.7)
+);
+carouselNextButton.addEventListener("click", () =>
+	carouselWhyFaveniScroll(0.7)
+);
+
+// Menu
 burgerButton.addEventListener("click", toggleMenu);
 menu.addEventListener("click", toggleMenu);
 
-document.body.addEventListener("click", bodyClickHandler);
+document.body.addEventListener("click", clickHandler);
+window.onresize = resizeHandler;
